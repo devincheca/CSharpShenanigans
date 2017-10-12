c65d0c9c8dbd948474ca95bf0662ab6f1fc17842
Devin Checa <devincheca@gmail.com>
2017-10-12 16:49:36 -0700

    Add files via upload

---
+++ 10-12-2017/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Execute.start();
+        }
+    }
+    public class Execute
+    {
+        public static void start()
+        {
+            Console.WriteLine("testing from Execute class\n");
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write(remainder(10,i) + " ");
+                Console.Write(parity(10, i) + "\n");
+            }
+            Console.ReadLine();
+        }
+        private static int remainder(int bigInt, int littleInt)
+        {
+            return littleInt % bigInt;
+        }
+        private static string parity(int bigInt, int littleInt)
+        {
+            bool odd = false;
+            if ((littleInt % bigInt) % 2 > 0)
+            {
+                odd = true;
+            }
+            return odd ? "odd" : "even";
+        }
+    }
+}
